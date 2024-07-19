c167196ef9c83f58b2a69782838ad49a848840a3
Faiz Febrianto <faizfebrianto06@gmail.com>
2024-07-19 01:31:07 -0700

    ubah judul

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lelang || Faiz</title>
+    <title>Lelang || Rahyan</title>
     <link rel="stylesheet" href="style5.css">
     <link rel="stylesheet" href="../admin/css/fontawesome/css/all.min.css">
 </head>
